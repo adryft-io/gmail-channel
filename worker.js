7024fe62104b3d108aa3ccd8543e1f06a9612694
Matthew Vargeson <mvargeson@Matthews-MacBook-Air.local>
2016-06-21 16:19:17 -0700

    fix worker bug data->messages

--- worker.js
+++ worker.js
@@ -16,28 +16,39 @@ var getToken = function(userId) {
   return tokens[userId];
 }
 
-
-sqs.createQueue({QueueName: 'triggers'}, function (err, resp) {
-  if (err) return console.log(err);
-  var url = resp.QueueUrl;
-  var queue = new AWS.SQS({params: {QueueUrl: url}});
-
-  connectedUsers.forEach(function(user) {
-    var token = getToken(user.id);
-    var gmail = new Gmail(token);
-    gmail.messages('after:1466518000', {format: 'metadata'})
-    .on('data', function (data) {
-      if(data.labelIds.includes('IMPORTANT')) {
-        trigger.call(queue, 'new-important');
-      }
-    });
-  });
-});
-
-function trigger(name) {
-  var body = JSON.stringify({ channel: 'gmail', name: name });
+var trigger = function(name, userId) {
+  var body = JSON.stringify({ channel: 'gmail', name: name, userId: userId });
   this.sendMessage({ MessageBody: body }, function (err, data) {
     if (err) return console.log(err);
     console.log(data.MessageId);
   });
 }
+
+// TODO: promsify
+sqs.getQueueUrl({QueueName: 'triggers'}, function (err, resp) {
+  if (err) return console.log(err);
+  var queue = new AWS.SQS({params: {QueueUrl: resp.QueueUrl}});
+  trigger = trigger.bind(queue);
+
+  connectedUsers.forEach(function(user) {
+    var messages = [];
+    var token = getToken(user.id);
+    var gmail = new Gmail(token);
+    gmail.messages('after:1466518000', {format: 'metadata'})
+    .on('data', function (data) {
+      messages.push(data);
+    })
+    .on('end', function() {
+      var occurred = {}
+      messages.forEach(function(message) {
+        if(message.labelIds.includes('IMPORTANT') && typeof occurred['new-important'] === 'undefined') {
+          occurred['new-important'] = true;
+        }
+      })
+
+      Object.keys(occurred).forEach(function(name) {
+        trigger(name, user.id);
+      });
+    });
+  });
+});
